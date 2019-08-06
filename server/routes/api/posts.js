7d74305ba494d4297bd2e919622738d40f609074
PABLO RODRIGUEZ <pablorodrigueza@hotmail.cl>
2019-08-06 16:38:11 -0400

    Initial backend created

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,13 +4,37 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // Get Posts
-router.get('/', (req, res) => {
-    res.send('hello');
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+
+    res.send(await posts.find({}).toArray());
 })
 
 // Add Post
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()        
+    });
 
+    res.status(201).send();
+});
 
 // Delete Post
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+async function loadPostsCollection(){
+    const client = await mongodb.MongoClient.connect
+    ('mongodb+srv://abc123:brad123456@cluster0-oruqb.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    });
+
+    return client.db('vue_express').collection('posts');
+}
 
 module.exports = router;
